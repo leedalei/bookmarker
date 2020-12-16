41c6123a53519001cb5f56fc860fdc13304481d5
leelei <543784988@qq.com>
2020-12-16 17:03:56 +0800

    更改打包设置，方便debug

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  devtool: 'cheap-module-source-map', //使用这个来防止打包后有eval，导致插件报错
+  devtool: 'source-map', //使用这个来防止打包后有eval，导致插件报错
   entry:  __dirname + "/js/index.js",
   output: {
     path: __dirname + "/dist",
