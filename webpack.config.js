76f0421f112ed8ea56f6557adb89ed400dccb521
leelei <543784988@qq.com>
2020-12-16 17:21:57 +0800

    加个babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  devtool: 'source-map', //使用这个来防止打包后有eval，导致插件报错
-  entry:  __dirname + "/js/index.js",
+  devtool: "source-map", //使用这个来防止打包后有eval，导致插件报错
+  entry: __dirname + "/js/index.js",
   output: {
     path: __dirname + "/dist",
-    filename: "bundle.js"
-  }
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 }
